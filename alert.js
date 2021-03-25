150ecafd0462b639b52368bacafa9aab9d1dec45
Jerrad J <jerrad.jefferson@yahoo.com>
2021-03-24 19:38:03 -0500

    javascript addition

--- alert.js
+++ alert.js
@@ -1,1 +1,14 @@
-alert ("Warning: St. Louis can be dangerous, be cautious and alert especially at night")
+<script type="text/javascript">
+stars = [0,0,2,3,5]
+
+var sum = stars.reduce(add, 0);
+function add(a, b) {
+    return a + b;
+}
+var text='';
+
+ for (i = 0; i < stars.length; i++) { 
+  text += "The percentege of the star "+(i+1)+ " is :"+ (stars[i]/sum)*100 + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+</script>
